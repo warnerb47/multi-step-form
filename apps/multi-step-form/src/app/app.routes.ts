845b08fc5c2933b298aebbd1a9dc0254dfe3c714
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-15 00:19:25 +0000

    feat: init routing

--- apps/multi-step-form/src/app/app.routes.ts
+++ apps/multi-step-form/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: AppComponent,
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'personal-info',
+            },
+            {
+                path: 'personal-info',
+                loadComponent: () => import('./views/personal-info/personalInfo.component').then(m => m.PersonalInfoComponent),
+            },
+            {
+                path: 'select-plan',
+                loadComponent: () => import('./views/select-plan/selectPlan.component').then(m => m.SelectPlanComponent),
+            },
+            {
+                path: 'pick-add-ons',
+                loadComponent: () => import('./views/pick-add-ons/pickAddOns.component').then(m => m.PickAddOnsComponent),
+            },
+            {
+                path: 'finishing-up',
+                loadComponent: () => import('./views/finishing-up/finishingUp.component').then(m => m.FinishingUpComponent),
+            },
+            {
+                path: 'finishing-up/thank-you',
+                loadComponent: () => import('./views/thank-you/thankYou.component').then(m => m.ThankYouComponent),
+            },
+            {
+                path: '**',
+                loadComponent: () => import('./views/not-found/not-found.component').then(m => m.NotFoundComponent),
+            }
+        ]
+    }
+];
