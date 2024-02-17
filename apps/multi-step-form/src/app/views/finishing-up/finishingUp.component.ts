bdd0cb955d7f55d2d7248d9f010faff7c3920fa6
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-17 00:05:32 +0000

    feat: integrate card component

--- apps/multi-step-form/src/app/views/finishing-up/finishingUp.component.ts
+++ apps/multi-step-form/src/app/views/finishing-up/finishingUp.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../components/card.component';
 
 @Component({
   selector: 'multi-step-form-finishing-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, CardComponent ],
   templateUrl: './finishingUp.component.html',
   styleUrl: './finishingUp.component.scss',
 })
--- apps/multi-step-form/src/app/views/personal-info/personalInfo.component.ts
+++ apps/multi-step-form/src/app/views/personal-info/personalInfo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../components/card.component';
 
 @Component({
   selector: 'multi-step-form-personal-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, CardComponent],
   templateUrl: './personalInfo.component.html',
   styleUrl: './personalInfo.component.scss',
 })
